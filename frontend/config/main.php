4f6e885ec23eef204574a99db19cbf6232a1eda0
Bibek <link.bibek123@gmail.com>
2018-02-20 22:40:12 +0545

    .htaccess added

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,29 +1,31 @@
 <?php
 $params = array_merge(
-    require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
 );
-
+ 
+use \yii\web\Request;
+$baseUrl = str_replace('/frontend/web', '', (new Request)->getBaseUrl());
+ 
 return [
     'id' => 'app-frontend',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+ 
     'components' => [
+ 
         'request' => [
-            'csrfParam' => '_csrf-frontend',
+            'baseUrl' => $baseUrl,
         ],
+ 
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
         ],
-        'session' => [
-            // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
-        ],
+ 
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
@@ -33,17 +35,17 @@ return [
                 ],
             ],
         ],
+ 
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
+ 
         'urlManager' => [
+            'baseUrl' => $baseUrl,
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
+            'rules' => []
+        ]
     ],
     'params' => $params,
 ];
